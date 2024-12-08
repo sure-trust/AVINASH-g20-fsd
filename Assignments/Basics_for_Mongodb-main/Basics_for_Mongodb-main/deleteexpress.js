14c752aeb94e9e8d0ccb30e6c2c59be80ed94fb3
Avinash2292 <d.avinashkumar22@gmail.com>
2024-12-08 09:43:58 +0530

    Delete Assignments directory

--- Assignments/Basics_for_Mongodb-main/Basics_for_Mongodb-main/deleteexpress.js
+++
@@ -1,9 +0,0 @@
-const connectdb = require("./connection");
-//Es is known as ECMAScript 
-const deletedata=async ()=>{
-    let data =await connectdb();
-    let result =await data.deleteOne({name:'XYZ'});
-    console.log("Data deleted successfully",result);
-
-}
-deletedata();
